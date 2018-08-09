ddd7460eafea6b9e2687fca202a3a8b299c5c5a4
Desktop\Richard <richard.sage28@gmail.com>
2018-08-08 21:37:02 -0300

    added empty tank movement component, adjusted values to get tank movement at least partially working

--- BattleTank 4.20/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank 4.20/Source/BattleTank/Private/TankTrack.cpp
@@ -3,10 +3,12 @@
 #include "TankTrack.h"
 
 void UTankTrack::SetThrottle(float Throttle) {
-	UE_LOG(LogTemp, Warning, TEXT("%f: throttle"), Throttle);
+	//UE_LOG(LogTemp, Warning, TEXT("%f: throttle"), Throttle);
 
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	UE_LOG(LogTemp, Warning, TEXT("%s: throttle"), *ForceApplied.ToString());
 	auto ForceLocation = GetComponentLocation();
+	UE_LOG(LogTemp, Warning, TEXT("%s: location"), *ForceLocation.ToString());
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
